c5309ff71d81d705e049c4f0198e077e87bcdf79
LinTongXue <LinTongXue@lin.com>
2023-01-07 10:12:21 +0800

    Two 本次增加了查询所有未抽查的单词，修复了随机抽查单词点击正确或错误会重复抽查的问题，增加了一些样式。

--- src/pages/layout/word.vue
+++ src/pages/layout/word.vue
@@ -2,7 +2,7 @@
 <template>
   <!--单词表格-->
   <div style="float: right">
-    <el-table v-loading="loading" size="large" border :data="tableData" style="width: 680px" max-height="88vh" @selection-change="getTheSelectedWordIds">
+    <el-table :empty-text="TableNoDataValue" v-loading="loading" size="large" border :data="tableData" style="width: 680px" max-height="88vh" @selection-change="getTheSelectedWordIds">
       <el-table-column fixed type="selection" width="55" align="center" header-align="center" />
       <el-table-column align="center" header-align="center" prop="word" label="单词" width="180" />
       <el-table-column align="center" :sortable="true" :sort-method="PageSort" header-align="center" prop="page" label="页数" width="90" />
@@ -49,7 +49,7 @@
           content="未抽查的单词"
           placement="left-start"
       >
-        <el-button><el-icon size="25px"><ZoomOut /></el-icon></el-button>
+        <el-button @click="wordsNotSelected"><el-icon size="25px"><ZoomOut /></el-icon></el-button>
       </el-tooltip>
     </div>
     <div style="text-align: center;margin-top: 50px;">
@@ -103,6 +103,18 @@
       </el-tooltip>
     </div>
   </div>
+  <div v-if="backAllWord" style="height: 100%;width: 60px;float: right;margin-right: 5px">
+    <div style="text-align: center;margin-top: 50px;">
+      <el-tooltip
+          class="box-item"
+          effect="dark"
+          content="返回所有单词"
+          placement="left-start"
+      >
+        <el-button @click="()=>{showWord().then(()=>{backAllWord = false;TASKBAR_POINTER = ''})}"><el-icon size="25px"><ZoomIn /></el-icon></el-button>
+      </el-tooltip>
+    </div>
+  </div>
 
   <!--抽查单词模块-->
   <div style="margin-left: 180px;margin-top: 90px">
@@ -115,7 +127,7 @@
         :active-icon="Check"
         :inactive-icon="Close"
     />
-    <el-table empty-text="请先设置单词范围~" border :data="spotCheckData" style="width: 660px">
+    <el-table :empty-text="spotCheckTableNoDataValue" border :data="spotCheckData" style="width: 660px">
       <el-table-column align="center" header-align="center" label="单词" width="180">
         <template #default="scope">
           <div style="">
@@ -142,8 +154,8 @@
   <!-- 抽查单词范围设置模块 -->
   <div v-if="switchValue" style="width: 550px;height: 250px;margin-top: 250px;margin-left: 250px;border:1px solid #ebeef5;text-align: center">
     <div style="font-family: 华文彩云,serif;font-size: 30px;margin-top: 20px">抽查范围设置</div>
-    <el-slider v-model="NoOne" show-input style="width: 450px;margin: auto;margin-top: 30px" :max="tableData.length"/>
-    <el-slider v-model="NoTwo" show-input style="width: 450px;margin: auto;margin-top: 40px" :max="tableData.length"/>
+    <el-slider v-model="NoOne" show-input style="width: 450px;margin: auto;margin-top: 30px" :max="maxLength"/>
+    <el-slider v-model="NoTwo" show-input style="width: 450px;margin: auto;margin-top: 40px" :max="maxLength"/>
     <el-button round style="margin-top: 20px;margin-left: 100px" @click="randomlyCheckWords">
       <el-icon size="20px" style="margin-right: 10px"><Watermelon /></el-icon>
       OK!
@@ -153,21 +165,66 @@
       Reset!
     </el-button>
   </div>
+
+  <div v-if="!switchValue" class="card" style="float: left;margin-top: 190px;margin-left: 390px;">
+    <div class="card-border-top">
+    </div>
+    <el-image class="img" src="https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/14753/tired.jpg" />
+    <span> LinTongXue</span>
+    <p class="job">You are enviable <br/>for your hard work</p>
+    <button> Click
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
 /*引入Vue3依赖*/
 import {getCurrentInstance, reactive, ref, watch} from "vue";
 import {ElMessage} from "element-plus";
-import { Check, Close } from '@element-plus/icons-vue'
+import {Check, Close, Timer} from '@element-plus/icons-vue'
 /*调用axios*/
 const currentInstance = getCurrentInstance()
 const {$http}: any = currentInstance?.appContext.config.globalProperties
 
 /*
+* describe: 创建范围设置的最大值
+* */
+let maxLength;
+
+/*
+* describe: 创建是否返回所有单词列表判别标识
+* */
+const backAllWord = ref(false)
+
+/*
+* describe: 创建工作栏列表各属性对应的名称列表
+* */
+const taskbar:string[] = ['UnselectedWords','CorrectWords','ErrorWords','FullyMasteredWords','HardToRememberWords'];
+
+/*
+* describe: 创建当前任务栏对象指针
+*   UnselectedWords：未被抽查的单词
+*   CorrectWords：正确的单词
+*   ErrorWords：错误的单词
+*   FullyMasteredWords：完全掌握的单词
+*   HardToRememberWords：难记的单词
+* */
+const TASKBAR_POINTER = ref('')
+
+/*
+* describe: 创建随机抽查单词没有数据时的提示语
+* */
+const spotCheckTableNoDataValue = ref("请先设置单词范围~");
+
+/*
+* describe: 创建单词列表没有数据时的提示语
+* */
+const TableNoDataValue = ref("没有单词数据");
+
+/*
 * describe: 创建switch开关判断标识
 * */
-const switchValue = ref(true);
+const switchValue = ref(false);
 
 /*
 * describe: 创建加载判断标识
@@ -225,6 +282,19 @@ const spotCheckData: word[] = reactive([
 ])
 
 /*
+* Function description: 睡眠函数
+* Function return value type: Promise<void>
+* Whether to call automatically: false
+* */
+const sleep = (millisecond:number) => {
+  return new Promise<void>(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, millisecond)
+  })
+}
+
+/*
 * Function description: 获取单词表中选中单词的id
 * Function return value type: void
 * Whether to call automatically: false
@@ -232,7 +302,6 @@ const spotCheckData: word[] = reactive([
 const getTheSelectedWordIds = (words:word[]) =>{
   wordIds.length=0;
   words.forEach((i:any) => wordIds.push(i.id));
-  console.log(wordIds)
 }
 
 /*
@@ -252,6 +321,7 @@ const PageSort = (a:word,b:word) => {
 const ResetSlider = () => {
   NoOne.value = 0;
   NoTwo.value = 0;
+  spotCheckTableNoDataValue.value = "请先设置单词范围~"
   spotCheckData.length=0;
 }
 
@@ -278,9 +348,10 @@ const trueCountSort = (a:any,b:any) => {
 * Function return value type: void
 * Whether to call automatically: false
 * */
-const randomlyCheckWordsToTrue = (row:any) => {
-  modifyWordTrue(row);
-  randomlyCheckWords();
+const randomlyCheckWordsToTrue = (row: any) => {
+  modifyWordTrue(row).then((v:any)=>{
+    randomlyCheckWords();
+  });
 }
 
 /*
@@ -289,8 +360,10 @@ const randomlyCheckWordsToTrue = (row:any) => {
 * Whether to call automatically: false
 * */
 const randomlyCheckWordsToError = (row:any) => {
-  modifyWordError(row);
-  randomlyCheckWords();
+  modifyWordError(row).then((v:any)=>{
+    randomlyCheckWords();
+  });
+
 }
 
 /*
@@ -298,10 +371,20 @@ const randomlyCheckWordsToError = (row:any) => {
 * Function return value type: void
 * Whether to call automatically: false
 * */
-const randomlyCheckWordsUtil = (NoOne:number,NoTwo:number) => {
-  $http.get("http://localhost:8081/word/WordNotCheckedId/" + NoOne + "," + NoTwo).then((res:any) => {
-    spotCheckData.length=0;
-    spotCheckData.push(res.data.data);
+const randomlyCheckWordsUtil = async (NoOne:number,NoTwo:number) => {
+  await $http.get("http://localhost:8081/word/spotCheckWord/" + NoOne + "," + NoTwo).then((res:any) => {
+    if(res.data.success == true) {
+      spotCheckData.length = 0;
+      spotCheckData.push(res.data.data);
+    }else{
+      spotCheckData.length = 0;
+      spotCheckTableNoDataValue.value = res.data.errorMsg;
+      ElMessage({
+        message: res.data.errorMsg,
+        grouping: true,
+        type: 'warning',
+      })
+    }
   })
 }
 
@@ -315,7 +398,7 @@ const randomlyCheckWordsUtil = (NoOne:number,NoTwo:number) => {
 *   then(res): 获取res中携带的修改信息使用弹窗显示出来
 * Whether to call automatically: false
 * */
-const randomlyCheckWords = () => {
+const randomlyCheckWords = async () => {
   if(NoOne.value > NoTwo.value && (NoOne.value !== 0 && NoTwo.value !== 0)){
     ElMessage({
       message: '第一个参数的值不能大于第二参数的值',
@@ -325,12 +408,12 @@ const randomlyCheckWords = () => {
   }else {
     if (NoOne.value === 0 && NoTwo.value !== 0) {
       NoOne.value = 1;
-      randomlyCheckWordsUtil(NoOne.value, NoTwo.value);
+      await randomlyCheckWordsUtil(NoOne.value, NoTwo.value);
     } else if (NoTwo.value === 0 && NoOne.value !== 0) {
       NoTwo.value = tableData.length;
-      randomlyCheckWordsUtil(NoOne.value, NoTwo.value);
+      await randomlyCheckWordsUtil(NoOne.value, NoTwo.value);
     } else if (NoOne.value !== 0 && NoTwo.value !== 0) {
-      randomlyCheckWordsUtil(NoOne.value, NoTwo.value);
+      await randomlyCheckWordsUtil(NoOne.value, NoTwo.value);
     } else {
       ElMessage({
         message: '请先设置范围',
@@ -359,7 +442,11 @@ const modifyWordTrueByIds = () => {
         grouping: true,
         type: 'success',
       })
-      showWord();
+      if(TASKBAR_POINTER.value === 'UnselectedWords') {
+        wordsNotSelected();
+        randomlyCheckWords();
+      }
+      else showWord();
     }else{
       ElMessage({
         message: res.data.errorMsg,
@@ -388,7 +475,11 @@ const modifyWordErrorByIds = () => {
         grouping: true,
         type: 'success',
       })
-      showWord();
+      if(TASKBAR_POINTER.value === 'UnselectedWords') {
+        wordsNotSelected();
+        randomlyCheckWords();
+      }
+      else showWord();
     }else{
       ElMessage({
         message: res.data.errorMsg,
@@ -409,15 +500,19 @@ const modifyWordErrorByIds = () => {
 *   then(res): 获取res中携带的修改信息使用弹窗显示出来
 * Whether to call automatically: false
 * */
-const modifyWordTrue = (row:any) => {
-  $http.post("http://localhost:8081/word/modifyWordTrue",row).then((res:any) => {
+const modifyWordTrue = async (row:any) => {
+  await $http.post("http://localhost:8081/word/modifyWordTrue",row).then((res:any) => {
     if(res.data.success === true){
       ElMessage({
         message: res.data.data,
         grouping: true,
         type: 'success',
       })
-      showWord();
+      if(TASKBAR_POINTER.value === 'UnselectedWords'){
+        wordsNotSelected();
+        randomlyCheckWords();
+      }
+      else showWord();
     }else{
       ElMessage({
         message: res.data.errorMsg,
@@ -438,15 +533,19 @@ const modifyWordTrue = (row:any) => {
 *   then(res): 获取res中携带的修改信息使用弹窗显示出来
 * Whether to call automatically: false
 * */
-const modifyWordError = (row:any) => {
-  $http.post("http://localhost:8081/word/modifyWordError",row).then((res:any) => {
+const modifyWordError = async (row:any) => {
+  await $http.post("http://localhost:8081/word/modifyWordError",row).then((res:any) => {
     if(res.data.success === true){
       ElMessage({
         message: res.data.data,
         grouping: true,
         type: 'success',
       })
-      showWord();
+      if(TASKBAR_POINTER.value === 'UnselectedWords'){
+        wordsNotSelected();
+        randomlyCheckWords();
+      }
+      else showWord();
     }else{
       ElMessage({
         message: res.data.errorMsg,
@@ -476,6 +575,8 @@ const reset = () => {
         type: 'success',
       })
       showWord();
+      ResetSlider();
+      TASKBAR_POINTER.value = '';
     }else{
       ElMessage({
         message: res.data.errorMsg,
@@ -483,6 +584,30 @@ const reset = () => {
         type: 'error',
       })
     }
+  })
+}
+
+/*
+* Function description: 利用axios获取后端返回的未抽查过的单词数据并给word数组赋值
+* Function return value type: void
+* axios:
+*   type: get
+*   port: http://localhost:8081/word/wordNotChecked/
+*   Value passed to backend: NoOne,NoTwo
+*   then(res): 获取res中data里的值赋值给tableData
+* Whether to call automatically: false
+* */
+const wordsNotSelected = () => {
+  $http.get('http://localhost:8081/word/wordNotChecked/' + NoOne.value + ',' + NoTwo.value).then((res:any) => {
+    if(res.data.success === true) {
+      tableData.length = 0;
+      res.data.data.forEach((i: any) => tableData.push(i));
+      TASKBAR_POINTER.value = taskbar[0];
+    }else{
+      tableData.length = 0;
+      TableNoDataValue.value = res.data.errorMsg
+    }
+    backAllWord.value = true;
   })
 }
 
@@ -496,12 +621,13 @@ const reset = () => {
 *   then(res): 获取res中data里的值赋值给tableData
 * Whether to call automatically: True
 * */
-const showWord = () => {
-  $http.get("http://localhost:8081/word/showWord").then((res:any) => {
+const showWord = async () => {
+  await $http.get("http://localhost:8081/word/showWord").then((res:any) => {
     if (res.data.success === true) {
       tableData.length = 0;
       res.data.data.forEach((i: word) => tableData.push(i));
       loading.value = false;
+      maxLength = tableData.length;
     }else{
       console.log(res.data.errorMsg)
     }
@@ -512,5 +638,65 @@ showWord();
 </script>
 
 <style scoped>
+.card {
+  text-align: center;
+  width: 250px;
+  height: 344px;
+  background: linear-gradient(to right top, #fb4372, #fc4264,#fc4359,#f94851,#f64943,#fc463c);
+  border-radius: 15px;
+  box-shadow: 1px 5px 60px 0 #100a886b;
+}
+
+.card .card-border-top {
+  width: 60%;
+  height: 3%;
+  background: #43cea4;
+  margin: auto;
+  border-radius: 0px 0px 15px 15px;
+}
+
+.card span {
+  font-weight: 600;
+  color: white;
+  text-align: center;
+  display: block;
+  padding-top: 25px;
+  font-size: 21px;
+}
+
+.card .job {
+  font-weight: 400;
+  color: white;
+  display: block;
+  text-align: center;
+  padding-top: 2px;
+  font-size: 14px;
+}
+
+.card .img {
+  width: 100px;
+  height: 110px;
+  background: #43cea4;
+  border-radius: 15px;
+  margin-left: auto;
+  margin-top: 35px;
+}
+
+.card button {
+  padding: 8px 25px;
+  display: block;
+  margin: auto;
+  border-radius: 8px;
+  border: none;
+  margin-top: 20px;
+  background: #4bc7e1;
+  color: #54625c;
+  font-weight: 600;
+}
+
+.card button:hover {
+  background: #ecf5ff;
+}
+
 
 </style>
